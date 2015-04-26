668aab235299cd3c8168e16658859a280e13c4b1
Grant Vinson <gvinson@liquidfish.com>
2015-04-26 22:21:02 +0000

    Added about page'

---
+++ app/views/about.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.master')
+@section('title')
+@parent About
+@stop
+@section('stylesheets')
+	
+@parent
+@stop
+
+@section('content')
+	
+	<div class="grid">
+		<div class="col-4">
+			<img src="/img/me.jpg" alt="Grant Vinson">
+			<div class="line"></div>
+			<ul>
+				<li><i class="icon-develop"></i>DEVELOPMENT</li>
+				<li><i class="icon-ux"></i>USER EXPERIENCE</li>
+				<li><i class="icon-mobile"></i>RESPONSIVE</li>
+				<li><i class="icon-design"></i>DESIGN</li>
+			</ul>
+		</div>
+		<div class="col-10 right">
+			<h1>How do you do,</h1>
+			<p><b>I'm Grant Vinson, a front end developer and UX designer currently located in Oklahoma City, OK.</b></p>
+			<p>Peoples' experiences on the web are what keep me up at night. Is this click area to close to this other click area? How does this font render on retina devices? Why is this page taking more than 2 seconds to load? These are the questions that make coffee mandatory.</p>
+			<p>With 4 years of professional experience under my belt and almost double that dabbling with programming, I know my way around websites. </p>
+			<p>I get out of this field what I put into it, hints why I am actively on sites like <a href="https://twitter.com/grant_vinson" target="_blank">Twitter</a>, <a href="https://github.com/gvinson" target="_blank">Github</a>, <a href="http://codepen.io/vinsongrant/" target="_blank">CodePen</a>, <a href="http://stackoverflow.com/users/1054156/grant-vinson" target="_blank">StackOverflow</a>, <a href="http://community.sitepoint.com/users/gvinson/activity" target="_blank">Sitepoint</a>; the more I can help, the more accomplished I am.</p>
+			<p>Creating top notch experiences are what matter to me. The ease of delivery for information to users all across the globe is what this industry is all about...that and coffee.</p>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+
+@stop
+
+@section('scripts')
+	@parent
+@stop
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -20,6 +20,12 @@
         <style>
             @font-face{font-family:Neutraface2Text-Demi;src:url(/fonts/Neutraface2Text-Demi.eot);src:url(/fonts/Neutraface2Text-Demi.eot?#iefix) format('embedded-opentype'),url(/fonts/Neutraface2Text-Demi.woff2) format('woff2'),url(/fonts/Neutraface2Text-Demi.woff) format('woff'),url(/fonts/Neutraface2Text-Demi.ttf) format('truetype'),url(/fonts/Neutraface2Text-Demi.svg#Neutraface2Text-Demi) format('svg');font-weight:400;font-style:normal;}@font-face{font-family:Neutraface2Text-Book;src:url(/fonts/Neutraface2Text-Book.eot);src:url(/fonts/Neutraface2Text-Book.eot?#iefix) format('embedded-opentype'),url(/fonts/Neutraface2Text-Book.woff2) format('woff2'),url(/fonts/Neutraface2Text-Book.woff) format('woff'),url(/fonts/Neutraface2Text-Book.ttf) format('truetype'),url(/fonts/Neutraface2Text-Book.svg#Neutraface2Text-Book) format('svg');font-weight:400;font-style:normal;}@font-face{font-family:Neutraface2Text-Bold;src:url(/fonts/Neutraface2Text-Bold.eot);src:url(/fonts/Neutraface2Text-Bold.eot?#iefix) format('embedded-opentype'),url(/fonts/Neutraface2Text-Bold.woff2) format('woff2'),url(/fonts/Neutraface2Text-Bold.woff) format('woff'),url(/fonts/Neutraface2Text-Bold.ttf) format('truetype'),url(/fonts/Neutraface2Text-Bold.svg#Neutraface2Text-Bold) format('svg');font-weight:400;font-style:normal;}@font-face{font-family:rbno2_lightregular;src:url(/fonts/rbno2light-webfont.eot);src:url(/fonts/rbno2light-webfont.eot?#iefix) format('embedded-opentype'),url(/fonts/rbno2light-webfont.woff2) format('woff2'),url(/fonts/rbno2light-webfont.woff) format('woff'),url(/fonts/rbno2light-webfont.ttf) format('truetype'),url(/fonts/rbno2light-webfont.svg#rbno2_lightregular) format('svg');font-weight:400;font-style:normal;}
         </style>
+        <script>
+            !function(e){function t(t,n,r,o){"use strict";function a(){for(var e,n=0;u.length>n;n++)u[n].href&&u[n].href.indexOf(t)>-1&&(e=!0);e?c.media=r||"all":setTimeout(a)}var c=e.document.createElement("link"),i=n||e.document.getElementsByTagName("script")[0],u=e.document.styleSheets;return c.rel="stylesheet",c.href=t,c.media="only x",c.onload=o||null,i.parentNode.insertBefore(c,i),a(),c}var n=function(r,o){"use strict";if(r&&3===r.length){var a=e.navigator,c=e.Image,i=!(!document.createElementNS||!document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect||!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1")||e.opera&&-1===a.userAgent.indexOf("Chrome")||-1!==a.userAgent.indexOf("Series40")),u=new c;u.onerror=function(){n.method="png",n.href=r[2],t(r[2])},u.onload=function(){var e=1===u.width&&1===u.height,a=r[e&&i?0:e?1:2];n.method=e&&i?"svg":e?"datapng":"png",n.href=a,t(a,null,null,o)},u.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==",document.documentElement.className+=" grunticon"}};n.loadCSS=t,e.grunticon=n}(this),function(e,t){"use strict";var n=t.document,r="grunticon:",o=function(e){if(n.attachEvent?"complete"===n.readyState:"loading"!==n.readyState)e();else{var t=!1;n.addEventListener("readystatechange",function(){t||(t=!0,e())},!1)}},a=function(e){return t.document.querySelector('link[href$="'+e+'"]')},c=function(e){var t,n,o,a,c,i,u={};if(t=e.sheet,!t)return u;n=t.cssRules?t.cssRules:t.rules;for(var l=0;n.length>l;l++)o=n[l].cssText,a=r+n[l].selectorText,c=o.split(");")[0].match(/US\-ASCII\,([^"']+)/),c&&c[1]&&(i=decodeURIComponent(c[1]),u[a]=i);return u},i=function(e){var t,o,a;o="data-grunticon-embed";for(var c in e)if(a=c.slice(r.length),t=n.querySelectorAll(a+"["+o+"]"),t.length)for(var i=0;t.length>i;i++)t[i].innerHTML=e[c],t[i].style.backgroundImage="none",t[i].removeAttribute(o);return t},u=function(t){"svg"===e.method&&o(function(){i(c(a(e.href))),"function"==typeof t&&t()})};e.embedIcons=i,e.getCSS=a,e.getIcons=c,e.ready=o,e.svgLoadedCallback=u,e.embedSVG=u}(grunticon,this);
+                        
+                grunticon(["/css/icons.data.svg.css", "/css/icons.data.png.css", "/css/icons.fallback.css"]);
+            </script>
+            <noscript><link href="/css/icons.fallback.css" rel="stylesheet"></noscript>
     @show
     <!--[if lt IE 9]>
         <script src="/js/vendor/html5shiv.min.js"></script>
@@ -42,6 +48,7 @@
         <a href="/" id="logo"><i class="icon-logo"></i></a>
         <nav class="top-nav">
             <ul>
+                <li><a href="/">HOME</a></li>
                 <li><a href="/work">WORK</a></li>
                 <li><a href="/about">ABOUT</a></li>
                 <li><a href="/contact">CONTACT</a></li>
@@ -51,9 +58,10 @@
    </header>
 
 
-
-    @section('content')
-    @show
+    <div id="content-wrapper">
+        @section('content')
+        @show
+    </div>
 
 </div>
 
