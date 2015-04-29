492d3e622d408245fd66a7c83cc28bb236df8192
Grant Vinson <gvinson@liquidfish.com>
2015-04-29 03:39:03 +0000

    Working on work page

--- app/views/work.blade.php
+++ app/views/work.blade.php
@@ -12,15 +12,60 @@
 	<div class="grid">
 		<div class="col-8">
 			<img src="/img/work/waterislife-overview.jpg">
+			<div class="overlay">
+				<div class="overlay-content">
+					<p class="site-name">WATERisLIFE</p>
+					<p class="tag">Responsibly created website with others in mind</p>
+					<div class="services">
+						<span class="icon-develop"></span>
+						<span class="icon-ux"></span>
+						<span class="icon-mobile"></span>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="col-8">
 			<img src="/img/work/vast-overview.jpg">
+			<div class="overlay">
+				<div class="overlay-content">
+					<p class="site-name">VAST</p>
+					<p class="tag">Eloquent and precise website with custom CMS</p>
+					<div class="services">
+						<span class="icon-develop"></span>
+						<span class="icon-ux"></span>
+						<span class="icon-mobile"></span>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="col-8">
 			<img src="/img/work/fellers-overview.jpg">
+			<div class="overlay">
+				<div class="overlay-content">
+					<p class="site-name">FELLERS</p>
+					<p class="tag">Large scale fully custom ecommerce website</p>
+					<div class="services">
+						<span class="icon-develop"></span>
+						<span class="icon-ux"></span>
+						<span class="icon-mobile"></span>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="col-8">
 			<img src="/img/work/regionalfoodbank-overview.jpg">
+			<div class="overlay">
+				<div class="overlay-content">
+					<p class="site-name">REGIONAL FOOD BANK</p>
+					<p class="tag">Non-profit dedicated to feeding everyone</p>
+					<div class="services">
+						<span class="icon-develop"></span>
+						<span class="icon-ux"></span>
+						<span class="icon-mobile"></span>
+						<span class="icon-design"></span>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="clearfix"></div>
 	</div>
